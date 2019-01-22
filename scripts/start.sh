2e3b3a792c5d1ea43d254d4b3cf31e025c0f39cf
ronzheng <ronzheng@none.com>
2019-01-22 15:10:33 +0800

    add scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,37 +3,8 @@
 ulimit -c unlimited
 echo core.%e.%p > /proc/sys/kernel/core_pattern
 
-export POS_HOME=/extp/pos
-export LD_LIBRARY_PATH=/extp/pos/lib:/usr/lib/extra:/usr/lib:/lib
+export DEMO_HOME=/opt/demo
+export LD_LIBRARY_PATH=${DEMO_HOME}/lib:/usr/lib:/lib
 
-rm -fr /dev/bgm/BIGMEM_PLAY
-rm -fr /dev/bgm/BIGMEM_PLAYLOCK
-rm -fr /dev/shm/SHMEM_PLAY
-
-rm -fr /dev/bgm/BIGMEM_LIVE
-rm -fr /dev/bgm/BIGMEM_LIVELOCK
-rm -fr /dev/shm/SHMEM_LIVE
-
-#
-# stage 1
-#
-
-./sysboot
-./msghub&
-./filemgr&
-
-#
-# stage 2
-#
-
-./camera&
-./viewer&
-
-#
-# state 3
-#
-
-./recorder&
-./player&
-#./mcu&
+./hello
 
